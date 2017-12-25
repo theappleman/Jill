516933e119c90a41fdb31ab6dc68793eb04e8937
ElenaChernaya <rbshayla@protonmail.com>
2017-12-25 16:34:22 +0100

    Added some comments

--- jill.py
+++ jill.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-configFile = open("config.txt", "r")
+configFile = open("config.txt", "r") #Replace that line with YOUR config.txt
 token = str(configFile.readline())
 
 def start(bot, update):
